007a614794f58a0876dce0ebcaa63e2cf461df4b
Yaqzan <shamvi.abdullah@gmail.com>
2025-12-13 18:30:38 +0600

    HTML

--- Project/TransferCred/transfercred.php
+++ Project/TransferCred/transfercred.php
@@ -4,5 +4,21 @@
 <title>Transfer Credits</title>
 </head>
 <body>
+<header> 
+<button id="btn">← Back</button>
+<h2>NeedSurveyResponses</h2>
+</header>
+<section>
+<h3>Transfer Credits</h3>
+<p>User ID: <b>XYZ</b><span style="margin-left:20px;">Credits: <b>42</b></span></p>
+Receiver User ID:<br>
+<input type="text" value="Enter user ID"><br><br>
+Confirm Receiver ID:<br>
+<input type="text" class="Re-enter user ID"><br><br>
+Amount to Transfer:<br>
+<input type="number" class="Enter transfer amount"><br><br>
+
+<button id="btncol">Confirm Transfer</button>
+</section>
 </body>
 </html>
