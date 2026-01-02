498e61dbce9d51d16b9147fdac823996ca282ca7
Yaqzan <shamvi.abdullah@gmail.com>
2026-01-03 00:44:10 +0600

    User profile database done, mostly taken from trans cred and admin view. Buttons dont work

--- Project/Profile/profilegui.php
+++ Project/Profile/profilegui.php
@@ -1,6 +1,16 @@
 <?php
 include "../db.php";
+
+$uid = 1;   // temp user
+$error = "";
+$success = "";
+$user_res = mysqli_query($conn, "SELECT * FROM users WHERE id = $uid");
+$user = mysqli_fetch_assoc($user_res);
+$profile_res = mysqli_query($conn, "SELECT * FROM profiles WHERE user_id = $uid");
+$profile = mysqli_fetch_assoc($profile_res);
+$camp_res = mysqli_query($conn, "SELECT * FROM campaigns WHERE user_id = $uid");
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,11 +27,14 @@ include "../db.php";
 <section>
 <div id= "box">
     <h3>My Profile</h3>
-    <p>User ID: <b id = "uid">XYZ</b><span style="margin-left: 15px;">Credits: <b>42</b></p>
-    Name:<br>
-    <input type="text" value="Student"><br><br>
+    <p>
+    User ID: <b id="uid"><?php echo $uid; ?></b>
+    <span style="margin-left: 15px;">
+    Credits: <b><?php echo $profile["credits"]; ?></b>
+    </span>
+    </p>
     Email:<br>
-    <input type="text" value="student@gmail.com"><br><br>
+    <input type="text" name="email" value="<?php echo $user["email"]; ?>">
     <button id ="btncol">Update Profile</button>
     <button style="background:red; color:white; cursor:pointer">Delete Account</button>
 </div>
@@ -37,14 +50,20 @@ include "../db.php";
 
 <div id = "box"> 
 <h3>My Surveys</h3>
-<div id="surveyItem">
-<b>Research Topic Selection Poll</b><br>
-<span id="meta">Responses: 3 / 10</span>
-</div>
-<div id="surveyItem">
-<b>Learning Habits Survey</b><br>
-<span id="meta">Responses: 10 / 10 (Closed)</span>
-</div>
+<?php
+while ($row = mysqli_fetch_assoc($camp_res))
+{
+    echo '<div id="surveyItem">';
+    echo '<b>' . $row["title"] . '</b><br>';
+    echo '<span id="meta">Responses: ' . $row["responses_served"] . ' / ' . $row["responses_needed"] . '</span>';
+    echo '</div>';
+}
+
+if (mysqli_num_rows($camp_res) == 0)
+{
+    echo '<span id="meta">No surveys created</span>';
+}
+?>
 </section>
 <footer>
 Contact us:
